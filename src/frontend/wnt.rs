a234133dfc50bd923bdfffdf4f7f3d805b872038
zehreken <zehreken@gmail.com>
2023-12-24 23:57:00 +0100

    Add a quad to render on

--- src/frontend/wnt.rs
+++ src/frontend/wnt.rs
@@ -40,28 +40,24 @@ impl Vertex {
 
 const VERTICES: &[Vertex] = &[
     Vertex {
-        position: [-0.0868241, 0.49240386, 0.0],
+        position: [-0.9, 0.9, 0.0],
         color: [0.5, 0.0, 0.5],
     }, // A
     Vertex {
-        position: [-0.49513406, 0.06958647, 0.0],
+        position: [-0.9, -0.9, 0.0],
         color: [0.5, 0.0, 0.5],
     }, // B
     Vertex {
-        position: [-0.21918549, -0.44939706, 0.0],
+        position: [0.9, -0.9, 0.0],
         color: [0.5, 0.0, 0.5],
     }, // C
     Vertex {
-        position: [0.35966998, -0.3473291, 0.0],
+        position: [0.9, 0.9, 0.0],
         color: [0.5, 0.0, 0.5],
     }, // D
-    Vertex {
-        position: [0.44147372, 0.2347359, 0.0],
-        color: [0.5, 0.0, 0.5],
-    }, // E
 ];
 
-const INDICES: &[u16] = &[0, 1, 4, 1, 2, 4, 2, 3, 4, /* padding */ 0];
+const INDICES: &[u16] = &[0, 1, 3, 1, 2, 3, /* padding */ 0];
 
 struct State {
     surface: wgpu::Surface,
