5b95b6de7be355a8a43da983810ac080b28ca708
zehreken <zehreken@gmail.com>
2024-09-14 13:54:43 +0200

    Refactor and cleanup

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,5 @@
 use std::collections::VecDeque;
 
-use wgpu::{SurfaceCapabilities, SurfaceError, TextureFormat};
 use winit::{
     dpi::{PhysicalSize, Size},
     event::{ElementState, Event, KeyEvent, WindowEvent},
@@ -15,17 +14,12 @@ use crate::{
 };
 
 pub struct App<'a> {
-    // surface: wgpu::Surface<'a>,
-    // device: wgpu::Device,
-    // queue: wgpu::Queue,
-    // config: wgpu::SurfaceConfiguration,
     size: winit::dpi::PhysicalSize<u32>,
     // The window must be declared after the surface so
     // it gets dropped after it as the surface contains
     // unsafe references to the window's resources.
     window: &'a Window, // this stays here but above goes to renderer
     renderer: renderer::Renderer<'a>,
-    // gui: gui::Gui,
     // cube: cube::State,
     triangle: triangle::State,
 }
@@ -33,33 +27,15 @@ pub struct App<'a> {
 impl<'a> App<'a> {
     async fn new(window: &'a Window) -> App<'a> {
         let size = window.inner_size();
-        // let (instance, surface) = create_instance_and_surface(window);
-        // let adapter = create_adapter(instance, &surface).await;
-        // let (device, queue) = create_device_and_queue(&adapter).await;
-
-        // let surface_caps = surface.get_capabilities(&adapter);
-        // let texture_format = surface_caps
-        //     .formats
-        //     .iter()
-        //     .copied()
-        //     .find(|f| f.is_srgb())
-        //     .unwrap_or(surface_caps.formats[0]);
-        // let surface_config = create_surface_config(size, texture_format, surface_caps);
-        // surface.configure(&device, &surface_config);
         let renderer = renderer::Renderer::new(window).await;
         let init = vec![0.0; 60];
-        // let gui = gui::Gui::new(window, &device, texture_format);
+
         // let cube = cube::State::new(&device, &surface_config);
         let triangle = triangle::State::new(&renderer.device);
         Self {
-            // surface,
-            // device,
-            // queue,
-            // config: surface_config,
             size,
             window: &window,
             renderer,
-            // gui,
             // cube,
             triangle,
         }
@@ -69,12 +45,8 @@ impl<'a> App<'a> {
         if size.width > 0 && size.height > 0 {
             self.size = size;
             self.renderer.resize(size, self.window.scale_factor());
-            // self.config.width = size.width;
-            // self.config.height = size.height;
-            // self.surface.configure(&self.device, &self.config);
             // self.cube.resize(size);
             self.triangle.resize(size);
-            // self.gui.resize(size, self.window.scale_factor());
         }
     }
 
@@ -85,73 +57,6 @@ impl<'a> App<'a> {
     fn update(&mut self) {
         // self.cube.update(&self.queue);
         // self.camera.update();
-    }
-
-    fn render(&mut self, fps: f32) -> Result<(), SurfaceError> {
-        /*
-        let output_frame = match self.surface.get_current_texture() {
-            Ok(frame) => frame,
-            Err(wgpu::SurfaceError::Outdated) => {
-                // This error occurs when the app is minimized on Windows.
-                // Silently return here to prevent spamming the console with:
-                // "The underlying surface has changed, and therefore the swap chain must be updated"
-                eprintln!("Outdated");
-                return Ok(());
-            }
-            Err(e) => {
-                eprintln!("Dropped frame with error: {}", e);
-                return Err(e);
-            }
-        };
-        let output_view = output_frame
-            .texture
-            .create_view(&wgpu::TextureViewDescriptor::default());
-
-        let mut encoder = self
-            .device
-            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
-                label: Some("renderer_encoder"),
-            });
-        let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-            label: Some("render_pass"),
-            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                view: &output_view,
-                resolve_target: None,
-                ops: wgpu::Operations {
-                    load: wgpu::LoadOp::Clear(wgpu::Color {
-                        r: 1.0,
-                        g: 0.0,
-                        b: 0.03,
-                        a: 1.0,
-                    }),
-                    store: wgpu::StoreOp::Store,
-                },
-            })],
-            depth_stencil_attachment: None,
-            timestamp_writes: None,
-            occlusion_query_set: None,
-        });
-        // cube
-        self.cube.update(&self.queue);
-        self.cube.render(&self.queue, &mut render_pass);
-        // triangle
-        self.triangle.render(&self.queue, &mut render_pass);
-
-        drop(render_pass);
-        self.gui.render(
-            &self.window,
-            &output_view,
-            &self.device,
-            &self.queue,
-            &mut encoder,
-            fps,
-        );
-        self.queue.submit(Some(encoder.finish()));
-        output_frame.present();
-        self.window.request_redraw();
-        Ok(())
-        */
-        Ok(())
     }
 }
 
@@ -204,8 +109,6 @@ fn run_event_loop(event_loop: EventLoop<()>, mut app: App) {
             rolling_frame_times.pop_front();
             rolling_frame_times.push_back(frame_time.as_secs_f32());
             let fps = calculate_fps(&rolling_frame_times);
-            // app.update();
-            // let _ = app.render(fps);
             let _ = app.renderer.render(&app.window, &app.triangle, fps);
             app.window.request_redraw();
         }
@@ -214,65 +117,6 @@ fn run_event_loop(event_loop: EventLoop<()>, mut app: App) {
         }
         _ => {}
     });
-}
-
-fn create_surface_config(
-    size: PhysicalSize<u32>,
-    texture_format: TextureFormat,
-    surface_caps: SurfaceCapabilities,
-) -> wgpu::SurfaceConfiguration {
-    let surface_config = wgpu::SurfaceConfiguration {
-        usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-        format: texture_format,
-        width: size.width,
-        height: size.height,
-        present_mode: surface_caps.present_modes[0],
-        alpha_mode: surface_caps.alpha_modes[0],
-        view_formats: vec![],
-        desired_maximum_frame_latency: 2,
-    };
-    surface_config
-}
-
-async fn create_device_and_queue(adapter: &wgpu::Adapter) -> (wgpu::Device, wgpu::Queue) {
-    let (device, queue) = adapter
-        .request_device(
-            &wgpu::DeviceDescriptor {
-                required_features: wgpu::Features::default(),
-                required_limits: wgpu::Limits::default(),
-                label: None,
-            },
-            None,
-        )
-        .await
-        .unwrap();
-    (device, queue)
-}
-
-async fn create_adapter(instance: wgpu::Instance, surface: &wgpu::Surface<'_>) -> wgpu::Adapter {
-    let adapter = instance
-        .request_adapter(&wgpu::RequestAdapterOptions {
-            power_preference: wgpu::PowerPreference::default(),
-            compatible_surface: Some(surface),
-            force_fallback_adapter: false,
-        })
-        .await
-        .unwrap();
-    adapter
-}
-
-fn create_instance_and_surface(
-    window: &winit::window::Window,
-) -> (wgpu::Instance, wgpu::Surface<'static>) {
-    let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
-        backends: wgpu::Backends::PRIMARY,
-        ..Default::default()
-    });
-    let surface = unsafe {
-        instance.create_surface_unsafe(wgpu::SurfaceTargetUnsafe::from_window(window).unwrap())
-    }
-    .unwrap();
-    (instance, surface)
 }
 
 fn create_window(size: Size, event_loop: &EventLoop<()>) -> winit::window::Window {
