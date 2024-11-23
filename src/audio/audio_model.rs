a353bd519965b31859fbd4472260e87e5fe5d9eb
zehreken <zehreken@gmail.com>
2024-11-23 21:09:01 +0100

    Refactor

--- src/audio/audio_model.rs
+++ src/audio/audio_model.rs
@@ -55,22 +55,8 @@ impl AudioModel {
         );
 
         // We'll try and use the same configuration between streams to keep it simple.
-        let config: cpal::StreamConfig = output_device.default_output_config().unwrap().into();
-
-        // Create a delay in case the input and output devices aren't synced.
-        // let latency_frames = (LATENCY_MS / 1_000.0) * config.sample_rate.0 as f32;
-        // let latency_samples = latency_frames as usize * config.channels as usize;
-
-        // The buffer to share samples
-        // let ring = HeapRb::new(4096);
-        // let (producer, mut consumer) = ring.split();
-
-        // Fill the samples with 0.0 equal to the length of the delay.
-        // for _ in 0..latency_samples {
-        //     // The ring buffer has twice as much space as necessary to add latency here,
-        //     // so this should never fail
-        //     producer.push(0.0).unwrap();
-        // }
+        let output_config: cpal::StreamConfig =
+            output_device.default_output_config().unwrap().into();
 
         let ring = HeapRb::new(4096 * 2);
         let (producer, mut consumer) = ring.split();
@@ -79,10 +65,10 @@ impl AudioModel {
         let view_ring = HeapRb::new(100000);
         let (view_producer, view_consumer) = view_ring.split();
 
-        let sample_rate = config.sample_rate.0;
+        let sample_rate = output_config.sample_rate.0;
         let audio_clock = Arc::new(AudioClock::new());
 
-        let metronome = Metronome::new(60, sample_rate, config.channels as u32);
+        let metronome = Metronome::new(60, sample_rate, output_config.channels as u32);
 
         let clock_for_audio = Arc::clone(&audio_clock);
         let output_data_fn = move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
@@ -99,10 +85,10 @@ impl AudioModel {
         // Build streams.
         println!(
             "Attempting to build both streams with f32 samples and `{:?}`.",
-            config
+            output_config
         );
         let output_stream = output_device
-            .build_output_stream(&config, output_data_fn, err_fn, None)
+            .build_output_stream(&output_config, output_data_fn, err_fn, None)
             .unwrap();
         println!("Successfully built streams.");
 
