e5dbee70ad11f40ed43f9e7381a5605d7027e100
zehreken <zehreken@gmail.com>
2025-06-09 23:39:43 +0200

    Improve maze.rs

--- src/misc/maze.rs
+++ src/misc/maze.rs
@@ -8,9 +8,18 @@ pub struct Cell {
     neighbors: Vec<u8>,
 }
 
+#[derive(Hash, PartialEq, Eq)]
 pub struct UndirectedEdge {
-    aId: u8,
-    bId: u8,
+    a_id: u8,
+    b_id: u8,
+}
+
+impl UndirectedEdge {
+    pub fn new(a: u8, b: u8) -> Self {
+        let (a_id, b_id) = if a < b { (a, b) } else { (b, a) };
+
+        Self { a_id, b_id }
+    }
 }
 
 #[test]
@@ -19,6 +28,7 @@ pub fn GenerateMaze() {
     const HEIGHT: usize = 3;
     let mut grid: [[u8; WIDTH]; HEIGHT] = [[0; WIDTH]; HEIGHT];
     let mut id_to_cell: HashMap<u8, Cell> = HashMap::new();
+    let mut edges: HashSet<UndirectedEdge> = HashSet::new();
 
     // left, top, right, bottom
     let neighbor: [(i8, i8); 4] = [(-1, 0), (0, -1), (1, 0), (0, 1)];
@@ -31,7 +41,10 @@ pub fn GenerateMaze() {
                 let n_column = column as i8 + coord.0;
                 let n_row = row as i8 + coord.1;
                 if n_column >= 0 && n_column < WIDTH as i8 && n_row >= 0 && n_row < HEIGHT as i8 {
-                    neighbors.push(unique_id(n_column as u8, n_row as u8, WIDTH as u8));
+                    let neighbor_id = unique_id(n_column as u8, n_row as u8, WIDTH as u8);
+                    neighbors.push(neighbor_id);
+                    let edge = UndirectedEdge::new(u_id, neighbor_id);
+                    edges.insert(edge);
                 }
             }
             let cell = Cell {
@@ -52,12 +65,13 @@ pub fn GenerateMaze() {
     dbg!(&grid);
     dbg!(&id_to_cell);
 
-    Dig(start_id, &id_to_cell, &mut visited, &mut frontier);
+    Dig(start_id, &id_to_cell, &edges, &mut visited, &mut frontier);
 }
 
 pub fn Dig(
     start_id: u8,
     id_to_cell: &HashMap<u8, Cell>,
+    edges: &HashSet<UndirectedEdge>,
     visited: &mut HashSet<u8>,
     frontier: &mut HashSet<u8>,
 ) {
@@ -65,7 +79,11 @@ pub fn Dig(
 
     for neighbor in &id_to_cell.get(&start_id).unwrap().neighbors {
         if !visited.contains(neighbor) {
-            Dig(*neighbor, id_to_cell, visited, frontier);
+            let edge = UndirectedEdge::new(start_id, *neighbor);
+            if edges.contains(&edge) {
+                dbg!("connected {} {}", start_id, *neighbor);
+            }
+            Dig(*neighbor, id_to_cell, edges, visited, frontier);
         }
     }
 }
