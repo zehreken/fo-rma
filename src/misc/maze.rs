44a73cddf8b56361e7d7e62b36b318236718f141
zehreken <zehreken@gmail.com>
2025-06-10 15:24:07 +0200

    Randomize neighbors

--- src/misc/maze.rs
+++ src/misc/maze.rs
@@ -1,3 +1,4 @@
+use rand::{rng, rngs::ThreadRng, seq::SliceRandom};
 use std::collections::{HashMap, HashSet};
 
 #[derive(Debug)]
@@ -8,13 +9,13 @@ pub struct Cell {
     neighbors: Vec<u8>,
 }
 
-#[derive(Hash, PartialEq, Eq)]
-pub struct UndirectedEdge {
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
+pub struct Edge {
     a_id: u8,
     b_id: u8,
 }
 
-impl UndirectedEdge {
+impl Edge {
     pub fn new(a: u8, b: u8) -> Self {
         let (a_id, b_id) = if a < b { (a, b) } else { (b, a) };
 
@@ -28,7 +29,9 @@ pub fn GenerateMaze() {
     const HEIGHT: usize = 3;
     let mut grid: [[u8; WIDTH]; HEIGHT] = [[0; WIDTH]; HEIGHT];
     let mut id_to_cell: HashMap<u8, Cell> = HashMap::new();
-    let mut edges: HashSet<UndirectedEdge> = HashSet::new();
+    let mut edges: HashSet<Edge> = HashSet::new();
+    let mut edge_to_connected: HashMap<Edge, bool> = HashMap::new();
+    let mut rng: ThreadRng = rng();
 
     // left, top, right, bottom
     let neighbor: [(i8, i8); 4] = [(-1, 0), (0, -1), (1, 0), (0, 1)];
@@ -43,10 +46,12 @@ pub fn GenerateMaze() {
                 if n_column >= 0 && n_column < WIDTH as i8 && n_row >= 0 && n_row < HEIGHT as i8 {
                     let neighbor_id = unique_id(n_column as u8, n_row as u8, WIDTH as u8);
                     neighbors.push(neighbor_id);
-                    let edge = UndirectedEdge::new(u_id, neighbor_id);
+                    let edge = Edge::new(u_id, neighbor_id);
                     edges.insert(edge);
+                    edge_to_connected.insert(edge, false);
                 }
             }
+            neighbors.shuffle(&mut rng);
             let cell = Cell {
                 id: u_id,
                 column: column as u8,
@@ -64,14 +69,25 @@ pub fn GenerateMaze() {
 
     dbg!(&grid);
     dbg!(&id_to_cell);
+    dbg!(&edges);
 
-    Dig(start_id, &id_to_cell, &edges, &mut visited, &mut frontier);
+    Dig(
+        start_id,
+        &id_to_cell,
+        &edges,
+        &mut edge_to_connected,
+        &mut visited,
+        &mut frontier,
+    );
+
+    dbg!(edge_to_connected);
 }
 
 pub fn Dig(
     start_id: u8,
     id_to_cell: &HashMap<u8, Cell>,
-    edges: &HashSet<UndirectedEdge>,
+    edges: &HashSet<Edge>,
+    edge_to_connected: &mut HashMap<Edge, bool>,
     visited: &mut HashSet<u8>,
     frontier: &mut HashSet<u8>,
 ) {
@@ -79,11 +95,19 @@ pub fn Dig(
 
     for neighbor in &id_to_cell.get(&start_id).unwrap().neighbors {
         if !visited.contains(neighbor) {
-            let edge = UndirectedEdge::new(start_id, *neighbor);
-            if edges.contains(&edge) {
+            let edge = Edge::new(start_id, *neighbor);
+            if edge_to_connected.contains_key(&edge) {
                 dbg!("connected {} {}", start_id, *neighbor);
+                *edge_to_connected.get_mut(&edge).unwrap() = true;
             }
-            Dig(*neighbor, id_to_cell, edges, visited, frontier);
+            Dig(
+                *neighbor,
+                id_to_cell,
+                edges,
+                edge_to_connected,
+                visited,
+                frontier,
+            );
         }
     }
 }
