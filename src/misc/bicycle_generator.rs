2aba96329232ee889189a1a5dab8da37c00a56ec
zehreken <zehreken@gmail.com>
2025-06-26 22:33:39 +0200

    Add comment for future reference

--- src/misc/bicycle_generator.rs
+++ src/misc/bicycle_generator.rs
@@ -1,3 +1,7 @@
+/// I want you to do some research about the Vitruvian man
+/// https://en.wikipedia.org/wiki/Vitruvian_Man
+/// It was a search for the proportions of ideal man
+/// A Vitruvian bike can be a similar search
 use crate::basics::scene_loader::SceneData;
 
 pub fn generate_bicycle_scene() -> SceneData {
