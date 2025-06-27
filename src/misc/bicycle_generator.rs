2c0bd326d4ee8f23cd34c5fa5b403340795ce3a5
zehreken <zehreken@gmail.com>
2025-06-27 23:13:46 +0200

    Add prototype code

--- src/misc/bicycle_generator.rs
+++ src/misc/bicycle_generator.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+
 /// I want you to do some research about the Vitruvian man
 /// https://en.wikipedia.org/wiki/Vitruvian_Man
 /// It was a search for the proportions of ideal man
@@ -6,4 +8,35 @@ use crate::basics::scene_loader::SceneData;
 
 pub fn generate_bicycle_scene() -> SceneData {
     todo!()
+}
+
+fn three_random_circles() {
+    let mut rng = rand::rng();
+    let a: Circle = Circle::new(
+        rng.random_range(-2.0..2.0),
+        rng.random_range(-2.0..2.0),
+        rng.random_range(2.0..5.0),
+    );
+    let b: Circle = Circle::new(
+        rng.random_range(-2.0..2.0),
+        rng.random_range(-2.0..2.0),
+        rng.random_range(2.0..5.0),
+    );
+    let c: Circle = Circle::new(
+        rng.random_range(-2.0..2.0),
+        rng.random_range(-2.0..2.0),
+        rng.random_range(2.0..5.0),
+    );
+}
+
+struct Circle {
+    pub x: f32,
+    pub y: f32,
+    pub r: f32,
+}
+
+impl Circle {
+    pub fn new(x: f32, y: f32, r: f32) -> Self {
+        Self { x, y, r }
+    }
 }
